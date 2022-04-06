1e5f4e6ae7c1d721825cb370bd41ea56faa823ea
avishoro <93486933+avishoro@users.noreply.github.com>
2022-04-06 11:39:28 +0300

    Create production-variables.tf

---
+++ terraform-code/modules/vm/production-variables.tf
@@ -0,0 +1,16 @@
+
+variable "projectPrefix" {
+  description = "The prefix which should be used for all resources connected to the web app."
+  default = "weight-tracker-app"
+}
+
+variable "location" {
+  description = "The supported Azure location where the resource exists."
+  default = "Switzerland North"
+}
+
+
+variable "num" {
+  description = "The number of  resources to create."
+  default = "3"
+}
